edc1792b26072fe83af868c17f7c2070e65f321d
graziabaiamonte <graziabaiamonte19@gmail.com>
2025-10-08 10:52:44 +0200

    add .gitignore

--- README.md
+++ README.md
@@ -8,4 +8,6 @@ L’app consente di:
 - Aggiungere, modificare e rimuovere compleanni
 - Ricevere promemoria per i compleanni in arrivo
 
-L’app è sviluppata con React 17 e utilizza **React Testing Library** e **Jest** per eseguire test unitari e di integrazione, garantendo la stabilità e la correttezza delle funzionalità principali.
+L’app è sviluppata con React 17 e utilizza **Webdriver.IO** per eseguire test diautomazione per browser e dispositivi mobili, garantendo la stabilità e la correttezza delle funzionalità principali.
+
+![Screenshot home](/public/Screenshot%202025-10-07%20alle%2012.13.42.png)
--- test/specs/visual.spec.js
+++ test/specs/visual.spec.js
@@ -1,13 +1,21 @@
-describe('My birthday reminders app', () => {
-	it('should look correctly', async () => {
-		await browser.url(``);
-		await browser.execute('/*@visual.init*/', 'Birthday Reminder App');
-		await browser.execute('/*@visual.snapshot*/', 'Default State');
+// con describe creo un gruppo di test
+describe("My birthday reminders app", () => {
+  // funzione async perché comandi asincroni con WebdriverIO come browser.url() e browser.execute()
+  it("should look correctly", async () => {
+    // vuoto ('') poichè punta all’app locale in esecuzione sullo stesso server di test.
+    await browser.url(``);
 
-		await $('[data-testid="clear"]').click();
-		await browser.execute('/*@visual.snapshot*/', 'Clear State');
+    // '/*@visual.init*/' è un marcatore per inizializzare il test visivo
+    await browser.execute("/*@visual.init*/", "Birthday Reminder App");
 
-		const result = await browser.execute('/*@visual.end*/');
-		expect(result.message).toBeNull();
-	});
+    // Questo comando scatta uno snapshot visivo dello stato corrente della pagina
+    await browser.execute("/*@visual.snapshot*/", "Default State");
+
+    await $('[data-testid="clear"]').click();
+    await browser.execute("/*@visual.snapshot*/", "Clear State");
+
+    // /*@visual.end*/ termina la sessione del test visivo
+    const result = await browser.execute("/*@visual.end*/");
+    expect(result.message).toBeNull();
+  });
 });
